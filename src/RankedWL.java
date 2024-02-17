0f92631d071ac176095b19639fd18cf7f7208412
paukl1337 <160300569+paukl1337@users.noreply.github.com>
2024-02-16 21:49:35 -0800

    fixed everything

--- src/RankedWL.java
+++ src/RankedWL.java
@@ -4,29 +4,41 @@
 
 /// logic:
 ///     request amount of wins / losses stored into an integer
-///     request data from api every 1 minute
+///     request data from api every 30 seconds
 ///     subtract wins/losses from wins/losses at start of session
+import java.io.*;
 import java.util.Scanner;
-import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Scanner;
 import org.json.JSONObject;
+
+import java.io.IOException;
+
 
 public class RankedWL
 {
+    String winFileName;
+    String lossFileName;
+    String eloRankFileName;
+    String eloFileName;
+    PrintWriter winFile, lossFile, eloRankFile, eloFile;
     Scanner webRead;
     String userInput;
-    String wins;
-    String losses;
-
+    int winsSes, lossesSes; // wins and losses for the session
+    int winsSeason, lossSeason; // total wins and losses for the season
+    int eloRank, eloRate; // elo rank and elo
+    // vars below are for file out
     public RankedWL()
     {
         webRead = null;
+        winFile = lossFile = eloRankFile = eloFile = null;
         userInput = "";
-        wins = "";
-        losses = "";
+        winFileName = "wins.txt";
+        lossFileName = "losses.txt";
+        eloRankFileName = "eloRank.txt";
+        eloFileName = "elo.txt";
+        winsSes = lossesSes = winsSeason = lossSeason = eloRank = eloRate = -1;
     }
 
     public static void main(String[] args)
@@ -38,24 +50,79 @@ public class RankedWL
     public void doIt()
     {
         getUserName();
-        doWork();
+        requestData();
 
     }
     public void getUserName()
     {
         Scanner kb = new Scanner(System.in);
+        System.out.print("Please enter the username to track! -> ");
         userInput = kb.next();
     }
     public void requestData()
     {
         Timer t = new Timer();
-        if(t.hasElapsed(30000, true))
+        doWork();
+        while(true)
         {
+            if(t.hasElapsed(30000, true))
+                doWork();
+        }
+
+    }
+    public void makeWinFile()
+    {
+        File file = new File(winFileName);
+        try
+        {
+            winFile = new PrintWriter(file);
+        } catch (IOException e)
+        {
+            System.err.println("ERROR: IOException caught! " + winFileName);
+            System.exit(1);
         }
     }
+    public void makeLossFile()
+    {
+        File file = new File(lossFileName);
+        try
+        {
+            lossFile = new PrintWriter(file);
+        } catch (IOException e)
+        {
+            System.err.println("ERROR: IOException caught! " + lossFileName);
+            System.exit(1);
+        }
+    }
+    public void makeEloFile()
+    {
+        File file = new File(eloFileName);
+        try
+        {
+            eloFile = new PrintWriter(file);
+        } catch (IOException e)
+        {
+            System.err.println("ERROR: IOException caught! " + eloFile);
+            System.exit(1);
+        }
+    }
+    public void makeEloRankFile()
+    {
+        File file = new File(eloRankFileName);
+        try
+        {
+            eloRankFile = new PrintWriter(file);
+        } catch (IOException e)
+        {
+            System.err.println("ERROR: IOException caught! " + eloRankFileName);
+            System.exit(1);
+        }
+    }
+
+
     public void doWork()
     {
-        String mcIgn = userInput; // Replace with actual IGN
+        String mcIgn = userInput;
         String apiUrl = "http://mcsrranked.com/api/users/" + mcIgn;
         try {
             URL url = new URL(apiUrl);
@@ -83,17 +150,32 @@ public class RankedWL
                 JSONObject season = statistics.getJSONObject("season");
                 JSONObject wins = season.getJSONObject("wins");
                 JSONObject loses = season.getJSONObject("loses");
-
-                String stats = String.format(
-                        "stats for %s - elo: %d | rank: %d | record: %d W - %d L",
-                        data.getString("nickname"),
-                        data.getInt("eloRate"),
-                        data.getInt("eloRank"),
-                        wins.getInt("ranked"),
-                        loses.getInt("ranked")
-                );
-
-                System.out.println(stats);
+                if(winsSeason == -1 && lossSeason == -1)
+                {
+                    winsSeason = wins.getInt("ranked");
+                    lossSeason = loses.getInt("ranked");
+                }
+                eloRank = data.getInt("eloRank");
+                eloRate = data.getInt("eloRate");
+                winsSes = wins.getInt("ranked") - winsSeason;
+                lossesSes = loses.getInt("ranked") - lossSeason;
+                System.out.printf("WINS: %d LOSSES: %d ELO: %d RANK: %d\n",
+                        winsSes,
+                        lossesSes,
+                        eloRate,
+                        eloRank);
+                makeWinFile();
+                makeLossFile();
+                makeEloFile();
+                makeEloRankFile();
+                winFile.println(winsSes);
+                winFile.close();
+                lossFile.println(lossesSes);
+                lossFile.close();
+                eloRankFile.println(eloRank);
+                eloRankFile.close();
+                eloFile.println(eloRate);
+                eloFile.close();
             }
 
         } catch (MalformedURLException e) {
@@ -124,7 +206,6 @@ class Timer
 
             return true;
         }
-
         return false;
     }
 
